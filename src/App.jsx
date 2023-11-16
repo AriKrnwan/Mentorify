a0786e6aa0e13aabce4dcd24609c855e2907b462
AriKrnwan <ari.20057@mhs.unesa.ac.id>
2023-11-17 01:27:12 +0700

    TES

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TypesExample from './components/button';
+import Navbar from './components/navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return <>
+    <Navbar />
+    <h1>AKU BUKANLAH SUPERMAN, AKU JUGA BISA NANGIS</h1>
+    <TypesExample />
+  </>
+}
+
+export default App
