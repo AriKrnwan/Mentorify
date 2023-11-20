63e95ada622ff1a54fa95d971b5997fc887a6693
AriKrnwan <ari.20057@mhs.unesa.ac.id>
2023-11-20 19:03:02 +0700

    KOMPONEN CARD SUDAH

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,51 @@
+import { Card } from "react-bootstrap"
+import { FiBookmark, FiBriefcase, FiStar } from 'react-icons/fi';
+import "../card/card.css"
+import PropTypes from 'prop-types';
+
+function CardMentor(props) {
+    const { nama, jenisMentor, ketTambah, harga, rating, foto } = props;
+
+    return(
+        <Card style={{ width: "18rem" }} className="col-3 gap-3">
+            <Card.Img variant="top" src={foto} />
+            <Card.Body className="p-0 gap-1">
+                <Card.Title className="d-flex justify-content-between align-items-start">
+                <h6 className="mb-0">{nama}</h6>
+                <FiBookmark size={20} />
+                </Card.Title>
+                <Card.Text className="d-flex flex-column gap-4">
+                <div className="info-mentor">
+                    <div className="info-mentor d-flex align-items-center">
+                    <FiBriefcase size={16} />
+                    <p className="mb-0 ms-2">{jenisMentor}</p>
+                    </div>
+                    <div className="info-mentor d-flex align-items-center">
+                    <FiStar fill="#E1C94A" stroke="none" size={16} />
+                    <p className="mb-0 ms-2">{ketTambah}</p>
+                    </div>
+                </div>
+                <div className="info-harga d-flex justify-content-between align-items-center">
+                    <h4 className="mb-0">Rp {harga}</h4>
+                    <div className="rating d-flex align-items-center px-2 py-1 rounded">
+                    <FiStar fill="#E1C94A" stroke="none" size={14} />
+                    <p className="mb-0">{rating}</p>
+                    </div>
+                </div>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    );
+}
+
+CardMentor.propTypes = {
+    nama: PropTypes.string.isRequired,
+    jenisMentor: PropTypes.string.isRequired,
+    ketTambah: PropTypes.string.isRequired,
+    harga: PropTypes.string.isRequired,
+    rating: PropTypes.string.isRequired,
+    foto: PropTypes.string.isRequired,
+};
+
+export default CardMentor
+
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,21 +3,39 @@ import "../home/Home.css"
 import profile from "../../assets/image/img-intro.svg";
 import ButtonWithIcon from '../../components/button/button';
 import { FiArrowRight } from 'react-icons/fi';
+import CardMentor from '../../components/card/card';
+import mentor1 from '../../assets/image/mentor1.png'
 
 const Home = () => {
     return ( 
         <>
             <Navbar/>
-            <div className="container-xxl row mx-auto">
-                <div className="col-6 text-start mt-5">
-                    <h1>Dapatkan Bimbingan dari Para Profesional.</h1>
-                    <p className='fs-6'>Aplikasi mentoring kami membantu anda mencapai potensi penuh anda dengan dukungan pribadi dari mentor berpengalaman, kurikulum yang disesuaikan, dan komunitas yang mendukung pertumbuhan Anda secara holistik.</p>
-                    <ButtonWithIcon text="Cari Mentor" icon={FiArrowRight} />
+            <div className="container-xxl mx-auto">
+                <div id='intro' className='row mx-auto mb-5'>
+                    <div className="col-6 text-start mt-5">
+                        <h1>Dapatkan Bimbingan dari Para Profesional.</h1>
+                        <p className='fs-6'>Aplikasi mentoring kami membantu anda mencapai potensi penuh anda dengan dukungan pribadi dari mentor berpengalaman, kurikulum yang disesuaikan, dan komunitas yang mendukung pertumbuhan Anda secara holistik.</p>
+                        <ButtonWithIcon text="Cari Mentor" icon={FiArrowRight} />
+                    </div>
+                    <div className="col-6 text-end">
+                        <img className='img-intro' src={profile} alt="Mentors" />
+                    </div>
                 </div>
-                <div className="col-6 text-end">
-                    <img className='img-intro' src={profile} alt="Mentors" />
+                <div id="recomend-mentors" className='d-flex justify-content-between row mx-auto'>
+                    <CardMentor 
+                        nama = "Aulia Safitri"
+                        jenisMentor = "Mentor Informatika"
+                        ketTambah = "Mentor Baru"
+                        harga = "5.000"
+                        rating = "4/5"
+                        foto = {mentor1}
+                    />
+                    <CardMentor />
+                    <CardMentor />
+                    <CardMentor />
                 </div>
             </div>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         </>
     );
 }
