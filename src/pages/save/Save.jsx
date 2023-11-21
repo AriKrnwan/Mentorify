a680fcec5a22047a0a7abb92e3798603ddd6b314
salim <inagbaka@gmail.com>
2023-11-21 14:40:34 +0700

    aku kerjain page save

--- src/pages/save/Save.jsx
+++ src/pages/save/Save.jsx
@@ -1,10 +1,23 @@
 import Navbar from'../../components/navbar/Navbar'
+import { Container, Row, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
+import Card from '../../components/card/Card'
 
 const Save = () => {
     return ( 
         <div>
             <Navbar/>
-            <h1>tersimpan</h1>
+            <Container fluid="xxl">
+                <Row className=' gap-3  justify-content-center  '>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                </Row>
+            </Container>   
         </div>
     );
 }
