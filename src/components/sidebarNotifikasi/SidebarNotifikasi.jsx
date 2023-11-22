917ff51412daac864f7fd39a45649895daa07f4e
salim <inagbaka@gmail.com>
2023-11-22 16:36:31 +0700

    ges tolong lanjutin page notif dong

--- src/components/sidebarNotifikasi/SidebarNotifikasi.jsx
+++ src/components/sidebarNotifikasi/SidebarNotifikasi.jsx
@@ -5,23 +5,25 @@ import { Col, Row } from "react-bootstrap";
 
 const SidebarNotifikasi = () => {
   return (
-    <div>
-      <Row>
-        <Col>
-          <div className="nav-notif d-flex flex-column gap-3 p-2">
-            <div className="heading-notif">Navigasi Profil</div>
-            <div className="mentoring-notif d-flex gap-3 align-items-center">
-              <FaRegUser size="24px" />
-              <span>Mentoring</span>
+    <Row >
+      <div className="position-fixed col-2" >
+        <Row>
+          <Col >
+            <div className="nav-notif d-flex flex-column gap-3 p-2 ">
+              <div className="heading-notif">Notifikasi</div>
+              <div className="mentoring-notif d-flex gap-3 align-items-center">
+                <FaRegUser size="24px" />
+                <span>Mentoring</span>
+              </div>
+              <div className="tanya-jawab-notif d-flex gap-3 align-items-center">
+                <PiClockCounterClockwiseBold size="24px" />
+                <span>Tanya & Jawab</span>
+              </div>
             </div>
-            <div className="tanya-jawab-notif d-flex gap-3 align-items-center">
-              <PiClockCounterClockwiseBold size="24px" />
-              <span>Tanya & Jawab</span>
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </div>
+          </Col>
+        </Row>
+      </div>
+    </Row>
   );
 };
 
--- src/pages/notification/Notification.jsx
+++ src/pages/notification/Notification.jsx
@@ -2,38 +2,27 @@ import Navbar from'../../components/navbar/Navbar'
 import { Container, Row, Col, Button, Card, ListGroup, Image } from 'react-bootstrap';
 import SidebarNotifikasi from '../../components/sidebarNotifikasi/SidebarNotifikasi';
 import picture from '../../assets/image/profile picture.jpg'
+import '../notification/Notification.css'
+import List from '../../components/listNotification/List';
 
 const Notification = () => {
     return ( 
         <div>
             <Navbar/>
-            <Container fluid= "xxl">
+            <Container fluid="xxl">
                 <Row className="gap-2">
-                    <Col md={3}>
-                    <Card>
-                        {/* <Card.Header>Notifikasi</Card.Header> */}
-                        <h2>notifikasi</h2>
-                        <Card.Body>
-                            <Button variant="primary">Mentoring</Button>
-                            <Button variant="primary">Tanya dan Jawab</Button>
-                        </Card.Body>
-                    </Card>
+                    <Col md={3} >
+                        <SidebarNotifikasi/>
                     </Col>
 
-                    <Col md={8}>
-                        <Card>
-                            {/* <Card.Header>Notifikasi Mentoring</Card.Header> */}
-                            <ListGroup variant="flush">
-                            <ListGroup.Item>
-                            <Card.Img variant="top" src="{picture}" />
-                                Permintaan mentoring diterima
-                                <br />
-                                <small>18-11-2023 14:07</small>
-                            </ListGroup.Item>
+                    <Col >
+                        <ListGroup >
+                            
                             <ListGroup.Item>
                                 Proses pembayaran selesai
                                 <br />
                                 <small>18-11-2023 14:07</small>
+                                <List />
                             </ListGroup.Item>
                             <ListGroup.Item>
                                 Mentoring sekarang
@@ -45,8 +34,63 @@ const Notification = () => {
                                 <br />
                                 <small>18-11-2023 14:07</small>
                             </ListGroup.Item>
-                            </ListGroup>
-                        </Card>
+                            <ListGroup.Item>
+                                Proses pembayaran kadaluarsa
+                                <br />
+                                <small>18-11-2023 14:07</small>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Proses pembayaran kadaluarsa
+                                <br />
+                                <small>18-11-2023 14:07</small>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Proses pembayaran kadaluarsa
+                                <br />
+                                <small>18-11-2023 14:07</small>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Proses pembayaran kadaluarsa
+                                <br />
+                                <small>18-11-2023 14:07</small>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Proses pembayaran kadaluarsa
+                                <br />
+                                <small>18-11-2023 14:07</small>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Proses pembayaran kadaluarsa
+                                <br />
+                                <small>18-11-2023 14:07</small>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Proses pembayaran kadaluarsa
+                                <br />
+                                <small>18-11-2023 14:07</small>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                Proses pembayaran kadaluarsa
+                                <br />
+                                <small>18-11-2023 14:07</small>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Proses pembayaran kadaluarsa
+                                <br />
+                                <small>18-11-2023 14:07</small>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Proses pembayaran kadaluarsa
+                                <br />
+                                <small>18-11-2023 14:07</small>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Proses pembayaran kadaluarsa
+                                <br />
+                                <small>18-11-2023 14:07</small>
+                            </ListGroup.Item>
+                        </ListGroup>
                     </Col>
                 </Row>
             </Container>
