8b11812841196c0ad8e901478a45c759559437b4
salim <inagbaka@gmail.com>
2023-11-28 15:12:42 +0700

    sudah

--- src/components/listNotificationMentoring/List.jsx
+++ src/components/listNotificationMentoring/List.jsx
@@ -1,26 +1,19 @@
 import '../listNotificationMentoring/List.css'
-import picture from '../../../src/assets/image/mentor4.png'
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
-const List = () => {
-
-    // function title() {
-    //     return <h4>Notifikasi Mentoring</h4>;
-    // }
-
-    // const title = "Notifikasi Mentoring";
+const List = ({judul,waktu,status,gambar}) => {
 
     return ( 
         <ListGroup className="mb-2">
             <ListGroupItem className='em'>
                 <div className='flex gap-2'>
                     <div >
-                        <img className='pic' src={picture} alt="picture" />
+                        <img className='pic' src={gambar} alt="picture" />
                     </div>
                     <div className='kontent'>
-                        <h6 className='judul'>permintaan mentor diterima</h6>
-                        <h6>permintaan mentor diterima</h6>
-                        <small>18-11-2023 18:57</small>
+                        <h6 className='judul'>{judul}</h6>
+                        <h6>{status}</h6>
+                        <small>{waktu}</small>
                     </div>
                 </div>
             </ListGroupItem>
--- src/pages/notification/Notification.jsx
+++ src/pages/notification/Notification.jsx
@@ -6,30 +6,20 @@ import ListQna from '../../components/listNotificationQ&a/listQna';
 import { FaRegUser } from "react-icons/fa6";
 import { PiClockCounterClockwiseBold } from "react-icons/pi";
 import { useState } from 'react';
+import profile_mentor from '../../assets/image/mentor2.png'
+import profile_user from '../../assets/image/profile_user.png'
 
 const Notification = () => {
 
-    // function change () {
-        // const [state, setState] = React.useState("Mentoring")
+    const [judul, setJudul] = useState("Notifikasi Mentoring")
 
-        // if (aria-selected=="true") {
-        //     <p>coba aja</p>
-        // }
+    const changeTextTop = () => {
+        setJudul('Notifikasi Tanya & Jawab');
+    };
 
-    // const [text, setText] = React.useState("mentoring");
-
-    // // function change () {
-    // //     if (menu == "mentoring") {
-    // //         return <h4>Halo</h4>;
-    // //     }
-    // //     if (menu == "forum") {
-    // //         return <h4>Hai</h4>;
-    // //     }
-    // // };
-
-    // const changeText = () => {
-    //     setText('Teks telah diubah!');
-    // };
+    const changeTextDown = () => {
+        setJudul('Notifikasi Mentoring');
+    };
 
     return ( 
         <div>
@@ -41,11 +31,11 @@ const Notification = () => {
                                 <h4>Notifikasi</h4>
                                     <Col sm={12} >
                                         <ListGroup className='gap-2'>
-                                            <ListGroup.Item className="try flex align-items-center gap-2" action href="#mentoring">
+                                            <ListGroup.Item onClick={changeTextDown} className="btn-notifikasi flex align-items-center gap-2" action href="#mentoring">
                                                 <FaRegUser/>
                                                 <div  className='huruf'>Mentoring</div>
                                             </ListGroup.Item>
-                                            <ListGroup.Item className="try flex align-items-center gap-2" action href="#qna">
+                                            <ListGroup.Item onClick={changeTextTop} className="btn-notifikasi flex align-items-center gap-2" action href="#qna">
                                                 <PiClockCounterClockwiseBold/>
                                                 <div className='huruf'>Tanya & Jawab</div>
                                             </ListGroup.Item>
@@ -53,33 +43,38 @@ const Notification = () => {
                                     </Col>
                             </Card>
                             <Col sm={9} className="mentor">
-                                <ListGroup className='border'>
-                                    <h4 className='title'>coba</h4>
+                                <ListGroup className='border-notifikasi-notif'>
+                                    <h4 className='judul-mentoring'>{judul}</h4>
                                     <Tab.Content>
-                                            <Tab.Pane eventKey="#mentoring">{<List/>}</Tab.Pane>
-                                            <Tab.Pane eventKey="#mentoring">{<List/>}</Tab.Pane>
-                                            <Tab.Pane eventKey="#mentoring">{<List/>}</Tab.Pane>
-                                            <Tab.Pane eventKey="#mentoring">{<List/>}</Tab.Pane>
-                                            <Tab.Pane eventKey="#mentoring">{<List/>}</Tab.Pane>
-                                            <Tab.Pane eventKey="#mentoring">{<List/>}</Tab.Pane>
-                                            <Tab.Pane eventKey="#mentoring">{<List/>}</Tab.Pane>
-                                            <Tab.Pane eventKey="#mentoring">{<List/>}</Tab.Pane>
-                                            <Tab.Pane eventKey="#mentoring">{<List/>}</Tab.Pane>
-                                            <Tab.Pane eventKey="#mentoring">{<List/>}</Tab.Pane>
-                                            <Tab.Pane eventKey="#mentoring">{<List/>}</Tab.Pane>
-                                            <Tab.Pane eventKey="#mentoring">{<List/>}</Tab.Pane>
-                                        <Tab.Pane eventKey="#qna">{<ListQna/>}</Tab.Pane>
-                                        <Tab.Pane eventKey="#qna">{<ListQna/>}</Tab.Pane>
-                                        <Tab.Pane eventKey="#qna">{<ListQna/>}</Tab.Pane>
-                                        <Tab.Pane eventKey="#qna">{<ListQna/>}</Tab.Pane>
-                                        <Tab.Pane eventKey="#qna">{<ListQna/>}</Tab.Pane>
-                                        <Tab.Pane eventKey="#qna">{<ListQna/>}</Tab.Pane>
-                                        <Tab.Pane eventKey="#qna">{<ListQna/>}</Tab.Pane>
-                                        <Tab.Pane eventKey="#qna">{<ListQna/>}</Tab.Pane>
-                                        <Tab.Pane eventKey="#qna">{<ListQna/>}</Tab.Pane>
-                                        <Tab.Pane eventKey="#qna">{<ListQna/>}</Tab.Pane>
-                                        <Tab.Pane eventKey="#qna">{<ListQna/>}</Tab.Pane>
-                                        <Tab.Pane eventKey="#qna">{<ListQna/>}</Tab.Pane>
+                                            <Tab.Pane eventKey="#mentoring">{<List 
+                                            judul="permintaan mentoring diterima"
+                                            status="permintaan mentoring kamu diterima, silakan lakukan proses proses pembayaran untuk memulai mentoring"
+                                            waktu="18-11-2023 14:07"
+                                            gambar={profile_mentor}/>}</Tab.Pane>
+                                            <Tab.Pane eventKey="#mentoring">{<List judul="permintaan mentor diterima"
+                                            waktu="putri jelek banget"
+                                            status="mentoring aman"/>}</Tab.Pane>
+                                            <Tab.Pane eventKey="#mentoring">{<List judul="permintaan mentor diterima"
+                                            waktu="putri jelek yeee aku jelekkkk"
+                                            status="mentoring aman"/>}</Tab.Pane>
+                                            <Tab.Pane eventKey="#mentoring">{<List judul="permintaan mentor diterima"
+                                            waktu="putri jelek hahahha"
+                                            status="mentoring aman"/>}</Tab.Pane>
+                                            <Tab.Pane eventKey="#mentoring">{<List judul="permintaan mentor diterima"
+                                            waktu="putri jelek banget bangettttttttt"
+                                            status="mentoring aman"/>}</Tab.Pane>
+                                        <Tab.Pane  eventKey="#qna">{<ListQna nama="Sambas Gaming"
+                                        waktu="18-11-2023 18:57"
+                                        keterangan="menyukai anda"
+                                        gambar={profile_user}/>}</Tab.Pane>
+                                        <Tab.Pane eventKey="#qna">{<ListQna nama="putri alifia rizky"
+                                        waktu="18-11-2023 18:57"
+                                        keterangan="menyukai anda"
+                                        gambar={profile_user}/>}</Tab.Pane>
+                                        <Tab.Pane eventKey="#qna">{<ListQna nama="Sambas Gaming"
+                                        waktu="18-11-2023 18:57"
+                                        keterangan="menyukai anda"
+                                        gambar={profile_user}/>}</Tab.Pane>
                                     </Tab.Content>
                                 </ListGroup>
                             </Col>
