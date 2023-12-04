826841a6b71dca9ced9aa45090dfd13759fdb43a
rehanahyuliantisp2 <rehanayulia37@gmail.com>
2023-12-04 23:51:52 +0700

    upload footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,21 +2,39 @@ import { NavLink } from "react-router-dom";
 import Logo from "../../assets/image/Logo.svg"
 
 const Footer = () => {
-    return ( 
-        <footer className='wrapper-footer d-flex flex-column align-items-center p-2 gap-4 mt-5 bg-body-secondary ' style={{fontFamily: "Poppins", fontWeight:"600", fontSize:"16px"}}>
-            <div className="wrapper-footer-logo">
-                <img src={Logo} alt="logo" />
-            </div>
-            <div className="navigation-footer d-flex gap-5">
-                <NavLink className="text-dark" to="/" >Home</NavLink >
-                <NavLink className="text-dark" to="/mentoring" >Mentoring</NavLink >
-                <NavLink className="text-dark" to="/qna" >Tanya & Jawab</NavLink >
-                <NavLink className="text-dark" to="/schedule" >Jadwal</NavLink >
-            </div>
-            <div className="wrapper-footer-text">
-                <p className="text-secondary ">Akatsuki 2023. All right reserved</p>
-            </div>
-        </footer>
+    return (
+      <footer className="wrapper-footer d-flex flex-column align-items-center p-2 gap-4 mt-5" style={{ fontFamily: "Poppins", fontWeight: "600", fontSize: "16px" }}>
+        <div className="d-flex flex-column align-items-center p-2 gap-4 mt-5">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1437" height="150" viewBox="0 0 1437 150" fill="none">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M-3 90.2778H57C117 90.2778 237 90.2778 357 70.1389C477 50 597 9.72222 717 9.72222C837 9.72222 957 50 1077 54.8611C1197 59.7222 1317 29.8611 1377 15.2778L1437 0V150H1377C1317 150 1197 150 1077 150C957 150 837 150 717 150C597 150 477 150 357 150C237 150 117 150 57 150H-3V90.2778Z"
+              fill="#F5F5F5"
+            />
+          </svg>
+          <div className="wrapper-footer-logo">
+            <img src={Logo} alt="logo" />
+          </div>
+          <div className="navigation-footer d-flex gap-5">
+            <NavLink className="text-dark" to="/">
+              Home
+            </NavLink>
+            <NavLink className="text-dark" to="/mentoring">
+              Mentoring
+            </NavLink>
+            <NavLink className="text-dark" to="/qna">
+              Tanya & Jawab
+            </NavLink>
+            <NavLink className="text-dark" to="/schedule">
+              Jadwal
+            </NavLink>
+          </div>
+          <div className="wrapper-footer-text">
+            <p className="text-secondary ">Akatsuki 2023. All right reserved</p>
+          </div>
+        </div>
+      </footer>
     );
 }
 
