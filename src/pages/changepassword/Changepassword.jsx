60065754a3a3180bdd77ecc53e28adb7da2008ae
Frhnalmsyh <farhanalamsyah47@gmail.com>
2023-12-18 16:05:47 +0700

    ganti kata sandi fe

--- src/pages/changepassword/Changepassword.jsx
+++ src/pages/changepassword/Changepassword.jsx
@@ -1,12 +1,52 @@
 // import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import '../changepassword/Changepassword.css'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import axios from 'axios';
 import Footer from '../../components/footer/Footer';
-// import { Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
+// import { Row } from 'react-bootstrap';
 const Changepassword = () => {
+  const [password, setPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState('');
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleNewPasswordChange = (e) => {
+    setNewPassword(e.target.value);
+  };
+
+  const handleConfirmNewPasswordChange = (e) => {
+    setConfirmNewPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const response = await axios.post('http://localhost:4121/Changepassword', {
+        password,
+        newPassword,
+        confirmNewPassword,
+      });
+  
+      console.log('Respon:', response.data);
+      window.alert('Kata sandi telah di ubah');
+      window.location.href='/'
+      // Handle keberhasilan atau logika lain di sini
+    } catch (error) {
+      console.error('Error:', error.response.data);
+      window.alert('Error: ${error.response.data.error}');
+      // Handle kesalahan atau logika lain di sini
+    }
+  };
+
+
+
   return (
     <div>
       <Navbar/> 
@@ -16,16 +56,19 @@ const Changepassword = () => {
           {/* <p className='wrapper-desc-form'>Anda akan keluar dari semua sesi kecuali sesi ini untuk melindungi akun Anda jika ada orang yang mencoba mendapatkan akses.
             <br></br><br></br>Kata sandi Anda minimal harus 6 karakter dan harus menyertakan kombinasi angka, huruf, dan karakter khusus (!$@%)</p> */}
           <div className='haha'>
-            <Form.Group className="input mb-3" controlId="formBasicEmail">
-              <Form.Control className="rounded-3 border-3" type="password" placeholder="Kata Sandi Lama" />
+            <Form.Group className="input mb-3" controlId="formBasicPassword1">
+              <Form.Control className="rounded-3 border-3" value={password}
+              onChange={handlePasswordChange} type="password" placeholder="Kata Sandi Lama" />
             </Form.Group>
-            <Form.Group className="input mb-3" controlId="formBasicPassword">
-              <Form.Control className='rounded-3 border-3' type="password" placeholder="Kata Sandi Baru" />
+            <Form.Group className="input mb-3" controlId="formBasicPassword2">
+              <Form.Control className='rounded-3 border-3'value={newPassword}
+              onChange={handleNewPasswordChange} type="password" placeholder="Kata Sandi Baru" />
             </Form.Group>
-            <Form.Group className="input mb-3" controlId="formBasicPassword">
-              <Form.Control className='rounded-3 border-3' type="password" placeholder="Konfirmasi Kata Sandi Baru" />
+            <Form.Group className="input mb-3" controlId="formBasicPassword3">
+              <Form.Control className='rounded-3 border-3'  value={confirmNewPassword}
+              onChange={handleConfirmNewPasswordChange} type="password" placeholder="Konfirmasi Kata Sandi Baru" />
             </Form.Group>
-            <Button className="btn-ganti" variant="primary" type="submit">
+            <Button className="btn-ganti" onClick={handleSubmit}>
               Ganti
             </Button>
           </div>
@@ -36,4 +79,4 @@ const Changepassword = () => {
   )
 }
 
-export default Changepassword;
+export default Changepassword; 
